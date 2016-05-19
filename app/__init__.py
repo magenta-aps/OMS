f7e332550dcf41dbfb78accf5853aac2574d50a5
Lanre <el_gigantes@hotmail.co.uk>
2016-05-19 12:12:19 +0200

    Testing properties file

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,33 @@
+import MySQLdb
 from flask import Flask
 from flask_cors.extension import CORS
 
 app = Flask(__name__)
 CORS(app)
-from app import db_connector
+
+global dbase
+
+def readSystemProps():
+    global sysProperties
+    sysProperties = {}
+    propsFile = "system/global.properties"
+    f = open(propsFile, 'r')
+    for line in f:
+        # print line
+        if(line[0] == '#'):
+            continue
+        a,b = line.split('=')
+        sysProperties[a]=b[:-1]
+
+readSystemProps()
+
+
+# Hard-coded DB settings until the properties stuff is done
+DB_HOST = sysProperties['db.connection.host']
+DB_USER = sysProperties['db.connection.user']
+DB_PWD = sysProperties['db.connection.password']
+DB_NAME = sysProperties['db.connection.name']
+
+dbase = MySQLdb.connect(DB_HOST, DB_USER, DB_PWD, DB_NAME)
+
+print "The db name is: ", DB_NAME
--- app/db_connector.py
+++ app/db_connector.py
@@ -3,12 +3,6 @@ __authors__ = 'lanre, andreas'
 from app import app
 from flask import jsonify, request, abort
 
-# Hard-coded DB settings until the properties stuff is done
-DB_HOST = 'localhost'
-DB_USER = 'andreas'
-DB_PWD = ''
-DB_NAME = 'oms'
-
 @app.route('/insert', methods = ['POST'])
 def insert_order():
     if not request.json:
@@ -17,3 +11,15 @@ def insert_order():
     items = request.json['items']
     
     return jsonify({"foo": "bar"})
+
+@app.route('/testdb', methods=['GET'])
+def getDbVersion():
+    # prepare a cursor object using cursor() method
+    cursor = dbase.cursor()
+    # execute SQL query using execute() method.
+    cursor.execute("SELECT VERSION()")
+    # Fetch a single row using fetchone() method.
+    data = cursor.fetchone()
+    #For now let's close the connection
+    dbase.close()
+    return jsonify({"version": "hurra"})
